f540313b53c8417713406cac8659127ac47ceec8
sandrinengo <sandrinengo@yahoo.com>
2023-03-30 20:29:48 -0400

    layout

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Prototype
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/main-theme.css",
                       "~/Content/site.css"));
         }
     }
